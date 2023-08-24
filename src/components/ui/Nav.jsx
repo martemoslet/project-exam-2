506116607ebbfc5de78a0dc85259d50bad8bdcd4
martemoslet <martemoslet@gmail.com>
2023-08-24 12:25:52 +0200

    Adds navbar

--- src/components/ui/Nav.jsx
+++ src/components/ui/Nav.jsx
@@ -1,7 +1,30 @@
-
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function Navbarnav() {
   return (
-    <></>
-  )
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">Holidaze</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/">Login</Nav.Link>
+            <NavDropdown title="Register" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#">Register as guest</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#">
+                Register as host
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }

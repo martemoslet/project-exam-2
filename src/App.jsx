35880ddb9908ed1e954c3404f43369067d969e79
martemoslet <martemoslet@gmail.com>
2023-08-24 12:25:39 +0200

    Adds routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+import Home from "./pages/home/Home";
+import VenuePage from "./pages/venue/VenuePage";
 
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-       
+        <Route index element={<Home />} />
+        <Route path="VenuePage/:id" element={<VenuePage />} />
       </Route>
     </Routes>
   );

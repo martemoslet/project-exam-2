43a15fd311df08096a4e79a297423b54479ecb9c
martemoslet <martemoslet@gmail.com>
2023-09-02 18:59:26 +0200

    Cleans up header

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,7 +1,12 @@
 import Navbarnav from "./Nav"
+import LoggedInNav from "./LoggedInNav";
+import * as storage from "../auth/storage"
+
+const profile = localStorage
 
 export default function Header() {
     return (<header>
-        <Navbarnav />
+ <Navbarnav />
+        
     </header>)
 }

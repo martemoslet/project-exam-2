0357f2743e24a091db39465b5e2ddb29090a6853
martemoslet <martemoslet@gmail.com>
2023-09-05 21:25:41 +0200

    Adds form and yup validation

--- src/pages/register/RegisterPage.jsx
+++ src/pages/register/RegisterPage.jsx
@@ -1,94 +1,86 @@
-import { useState } from "react";
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import { authFetch } from "../../components/auth/authFetch";
 import { useNavigate } from 'react-router-dom';
 
+const schema = yup
+.object({
+name: yup
+.string()
+.max(20, "Your name can be maximum 20 characters")
+.required("Please enter your full name"),
+email: yup
+.string()
+.matches(/^[\w\-.]+@(stud\.)?noroff\.no$/, "Only a stud.noroff.no email address can register")
+.required(),
+avatar: yup
+.string(),
+password: yup
+.string()
+.min(8, "Your password must be at least 8 characters")
+.required("Please enter a password"),
+venuManager: yup
+.boolean(),
+})
+.required();
+
+
 export default function Register() {
-const [name, setName] = useState('');
-const [email, setEmail] = useState('');
-const [avatar, setAvatar] = useState('');
-const [password, setPassword] = useState('');
-const [venueManager, setVenueManager] = useState(false);
+const {
+register,
+handleSubmit,
+formState: { errors },
+} = useForm({
+resolver: yupResolver(schema),
+});
 const navigate = useNavigate();
 
 
-async function onFormSubmit(event) {
-event.preventDefault();
-const profile = {
-name,
-email,
-avatar,
-password,
-venueManager,
-};
-
-const response = await authFetch('https://api.noroff.dev/api/v1/holidaze/auth/register', {
+async function onSubmit(data) {
+  const response = await authFetch('https://api.noroff.dev/api/v1/holidaze/auth/register', {
     headers: {
-        "Content-Type": "application/json",
+    "Content-Type": "application/json",
     },
     method: 'POST',
-    body: JSON.stringify(profile)
-});
-
-const result = await response.json()
-if (response.status === 201 || 204) {
+    body: JSON.stringify(data)
+    });
+    
+    
+    const result = await response.json()
+    if (response.status === 201 || 204) {
     //alert("Registration successful");
     console.log(result)
     navigate('/loginPage')
     return result;
-    
-  } else {
+    } else {
     alert("Something went wrong");
-  }
-}
-
-
-function onNameChange(event) {
-setName(event.target.value);
-}
-function onEmailChange(event) {
-setEmail(event.target.value);
-}
-function onAvatarChange(event) {
-setAvatar(event.target.value);
-}
-function onPasswordChange(event) {
-setPassword(event.target.value);
-}
-function onVenueManagerChange(event) {
-    setVenueManager(event.target.value);
     }
+    
+
+}
 
 
 return (
-<div>
-<form onSubmit={onFormSubmit}>
+<form className="contact-form" onSubmit={handleSubmit(onSubmit)} id="registerForm">
 <h1>Register</h1>
 <label htmlFor="name">Full name</label>
-<input name="name" value={name} onChange={onNameChange} />
-
-
+<input {...register("name")} />
+<p>{errors.name?.message}</p>
 <label htmlFor="email">Email address</label>
-<input name="email" value={email} onChange={onEmailChange} />
-
-
+<input {...register("email")} />
+<p>{errors.email?.message}</p>
 <label htmlFor="avatar">Avatar</label>
-<input name="avatar" value={avatar} onChange={onAvatarChange} />
+<input {...register("avatar")} />
+<p>{errors.avatar?.message}</p>
 <label htmlFor="passowrd">Password</label>
-<input name="password" value={password} onChange={onPasswordChange} type="password" />
-
-
+<input {...register("password")} type="password" />
+<p>{errors.password?.message}</p>
 <div>
-<input name="venueManager" type="checkbox" id='venueManager' className='me-2' onChange={onVenueManagerChange} 
-value={venueManager} />
-<label htmlFor="venueManager" id="venueManager">I want to register as a venue manager</label>
+<input type="checkbox" id='venueManager' className='me-2' {...register("venueManager")} />
+<label htmlFor="venueManager">I want to register as a venue manager</label>
 </div>
-
-
 <input type="submit" />
 </form>
-
-
-</div>
 );
-
 }

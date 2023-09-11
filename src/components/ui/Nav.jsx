58f7609a0f7fd12dab16619f69fd43085588389a
martemoslet <martemoslet@gmail.com>
2023-09-11 14:48:21 +0200

    Adds link to add venue page

--- src/components/ui/Nav.jsx
+++ src/components/ui/Nav.jsx
@@ -21,7 +21,8 @@ export default function Navbarnav() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>            
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/addVenuePage">Add venue</Nav.Link>         
 
             { profile ?
             <div>

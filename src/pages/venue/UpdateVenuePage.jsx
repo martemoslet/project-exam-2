0fb196b5073a5a566a5f5b62e7c4aca894299124
martemoslet <martemoslet@gmail.com>
2023-09-16 14:29:09 +0200

    Adds fetch venue data

--- src/pages/venue/UpdateVenuePage.jsx
+++ src/pages/venue/UpdateVenuePage.jsx
@@ -5,7 +5,8 @@ import { authFetch } from "../../components/auth/authFetch";
 import { useNavigate } from 'react-router-dom';
 import { useParams } from "react-router-dom";
 import RemoveVenue from './DeleteVenue';
-
+import { useEffect, useState } from 'react';
+import Spinner from 'react-bootstrap/Spinner';
 
 const schema = yup
 .object({
@@ -50,6 +51,7 @@ location: yup
 })
 .required();
 
+
 export default function UpdateVenue() {
 const {
 register,
@@ -61,6 +63,43 @@ resolver: yupResolver(schema),
 let { id } = useParams();
 //const navigate = useNavigate();
 
+const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+
+    
+    useEffect(() => {
+    async function getData(url) {
+    try {
+    setIsLoading(true);
+    setIsError(false);
+    
+    const response = await fetch(url);
+    const json = await response.json();
+    
+    
+    setData(json);
+    } catch (error) {
+    console.log(error);
+    } finally {
+    setIsLoading(false);
+    }
+    }
+    
+    getData(`https://api.noroff.dev/api/v1/holidaze/venues/${id}`);
+    }, [id]);
+    
+    if (isLoading || !data) {
+    return <Spinner animation="border" role="status">
+    <span className="visually-hidden">Loading...</span>
+    </Spinner>;
+    }
+    
+    
+    if (isError) {
+    return <div>Error</div>;
+    }
+    console.log(data)
 
 async function onSubmit(data) {
     data.media = data.media.split(",");
@@ -88,53 +127,53 @@ return (
 <form className="contact-form" onSubmit={handleSubmit(onSubmit)} id="registerForm">
 <h1>Update venue</h1>
 <label htmlFor="name">Venue name</label>
-<input {...register("name")} />
+<input {...register("name")} defaultValue={data.name} />
 <p>{errors.name?.message}</p>
 
 <label htmlFor="location.address">Address</label>
-<input {...register("location.address")} type="location.address" />
+<input {...register("location.address")} type="location.address" defaultValue={data.location.address} />
 
 <label htmlFor="location.city">City</label>
-<input {...register("location.city")} type="location.city" />
+<input {...register("location.city")} type="location.city" defaultValue={data.location.city} />
 
 <label htmlFor="location.zip">Zip code</label>
-<input {...register("location.zip")} type="location.zip" />
+<input {...register("location.zip")} type="location.zip" defaultValue={data.location.zip} />
 
 <label htmlFor="location.country">Country</label>
-<input {...register("location.country")} type="location.country" />
+<input {...register("location.country")} type="location.country" defaultValue={data.location.country} />
 
 <label htmlFor="location.continent">Continent</label>
-<input {...register("location.continent")} type="location.continent" />
+<input {...register("location.continent")} type="location.continent" defaultValue={data.location.continent} />
 
 <label htmlFor="description">Description</label>
-<textarea {...register("description")} />
+<textarea {...register("description")} defaultValue={data.description}/>
 <p>{errors.description?.message}</p>
 
 <label htmlFor="media">Media</label>
-<input type="url" {...register("media")} />
+<input type="url" {...register("media")} defaultValue={data.media} />
 
 <label htmlFor="price">Price</label>
-<input {...register("price")} type="price" />
+<input {...register("price")} type="price" defaultValue={data.price} />
 <p>{errors.price?.message}</p>
 
 <label htmlFor="maxGuests">Max guests</label>
-<input {...register("maxGuests")} type="maxGuests" />
+<input {...register("maxGuests")} type="maxGuests" defaultValue={data.maxGuests} />
 <p>{errors.maxGuests?.message}</p>
 
 <label htmlFor="rating">Rating</label>
-<input {...register("rating")} type="rating" />
+<input {...register("rating")} type="rating" defaultValue={data.rating} />
 
 <label htmlFor="meta.wifi">Wifi included</label>
-<input type="checkbox" className='' {...register("meta.wifi")} />
+<input type="checkbox" className='' {...register("meta.wifi")} defaultChecked={data.meta.wifi > 0 ? "true" : ""} />
 
 <label htmlFor="meta.parking">Parking on site</label>
-<input type="checkbox" className='' {...register("meta.parking")} />
+<input type="checkbox" className='' {...register("meta.parking")} defaultChecked={data.meta.parking > 0 ? "true" : ""} />
 
 <label htmlFor="meta.breakfast">Breakfast included</label>
-<input type="checkbox" className='' {...register("meta.breakfast")} />
+<input type="checkbox" className='' {...register("meta.breakfast")} defaultChecked={data.meta.breakfast > 0 ? "true" : ""} />
 
 <label htmlFor="meta.pets">Pets allowed</label>
-<input type="checkbox" className='' {...register("meta.pets")} />
+<input type="checkbox" className='' {...register("meta.pets")} defaultChecked={data.meta.pets > 0 ? "true" : ""} />
 
 <input type="submit" />
 </form>

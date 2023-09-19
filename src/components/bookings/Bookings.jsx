a5ff4dfda8d8b90ba7bb26503427be9bbd717c23
martemoslet <martemoslet@gmail.com>
2023-09-19 12:32:31 +0200

    Changes function name

--- src/components/bookings/Bookings.jsx
+++ src/components/bookings/Bookings.jsx
@@ -1,7 +1,10 @@
-import Booking from "./Booking";
+import BookingOnYourVenue from "./BookingOnYourVenue";
 
 export default function Bookings({ bookings }) {
   return (
-    bookings && bookings.map((booking) => <Booking key={booking.id} {...booking} />)
+    bookings &&
+    bookings.map((booking) => (
+      <BookingOnYourVenue key={booking.id} {...booking} />
+    ))
   );
 }

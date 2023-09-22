1aba4ace29d2ddf0fd852b320b075130702d4010
martemoslet <martemoslet@gmail.com>
2023-09-22 13:27:20 +0200

    Updates styling

--- src/components/profile/BookingsOnProfile.jsx
+++ src/components/profile/BookingsOnProfile.jsx
@@ -1,15 +1,13 @@
-import Row from 'react-bootstrap/Row';
-import Container from "react-bootstrap/Container";
-import { ProfileBookings } from './Bookings';
+import { ProfileBookings } from "./Bookings";
 
 export default function BookingsOnProfile({ bookings }) {
-    return (
-        <Container>
-            <Row>
-            {bookings && bookings.length && bookings.map((bookingData) => (
-                <ProfileBookings key={bookingData.id} bookingData={bookingData} />
-            ))}
-        </Row>
-        </Container>
-    )
+  return (
+    <>
+      {bookings &&
+        bookings.length &&
+        bookings.map((bookingData) => (
+          <ProfileBookings key={bookingData.id} bookingData={bookingData} />
+        ))}
+    </>
+  );
 }

42f2539f9e0be8a14cae81d29a7933b4e17cac60
martemoslet <martemoslet@gmail.com>
2023-09-22 13:28:37 +0200

    Adds styling

--- src/components/profile/VenuesOnProfile.jsx
+++ src/components/profile/VenuesOnProfile.jsx
@@ -1,15 +1,13 @@
-import Row from 'react-bootstrap/Row';
-import Container from "react-bootstrap/Container";
-import { ProfileVenues } from './Venues';
+import { ProfileVenues } from "./Venues";
 
 export default function VenuesOnProfile({ venues }) {
-    return (
-        <Container>
-            <Row>
-            {venues && venues.length && venues.map((venueData) => (
-                <ProfileVenues key={venueData.id} venueData={venueData} />
-            ))}
-        </Row>
-        </Container>
-    )
+  return (
+    <>
+      {venues &&
+        venues.length &&
+        venues.map((venueData) => (
+          <ProfileVenues key={venueData.id} venueData={venueData} />
+        ))}
+    </>
+  );
 }

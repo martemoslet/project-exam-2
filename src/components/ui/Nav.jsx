4068d531d566b25beec13ac1cbd33e2c0858be1e
martemoslet <martemoslet@gmail.com>
2023-09-22 13:29:07 +0200

    Adds styling on nav

--- src/components/ui/Nav.jsx
+++ src/components/ui/Nav.jsx
@@ -1,41 +1,41 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import * as storage from "../auth/storage"
-import { Link } from 'react-router-dom';
-import Logout from '../profile/Logout';
-import { useState, createContext } from "react";
-import { FaRegUser } from 'react-icons/fa';
-
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import * as storage from "../auth/storage";
+import { Link } from "react-router-dom";
+import Logout from "../profile/Logout";
+import { FaRegUser } from "react-icons/fa";
 
 const profile = storage.load("profile");
 
 export default function Navbarnav() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="white" expand="lg">
       <Container>
-        <Navbar.Brand href="/">Holidaze</Navbar.Brand>
+        <Navbar.Brand href="/" className="navLogo">
+          Holidaze
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/addVenuePage">Add venue</Nav.Link>         
+            {profile ? (
+              <Navbar.Collapse className="justify-content-end">
+                <Navbar.Text>
+                  <Link to={`/profilePage/${profile.name}`}>
+                    <FaRegUser size={18} className="me-2" />
+                  </Link>
 
-            { profile ?
-            <div>
-              <Link to={`/profilePage/${profile.name}`}><FaRegUser size={20} className="me-2" /></Link>
-              <Nav.Link href="#"><Logout /></Nav.Link>
-            </div> 
-            
-            :
-            <div>
-            <Nav.Link href="/loginPage">Login</Nav.Link>
-            <Nav.Link href="/registerPage">Register</Nav.Link>
-            </div>}
-
+                  <Logout />
+                </Navbar.Text>
+              </Navbar.Collapse>
+            ) : (
+              <Navbar.Collapse className="justify-content-end">
+                <Nav.Link href="/loginPage">Login</Nav.Link>
+                <Nav.Link href="/registerPage">Register</Nav.Link>
+              </Navbar.Collapse>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>

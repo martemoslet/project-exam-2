11e405807eed560fdab4d5f06d66d12e077e9acc
martemoslet <martemoslet@gmail.com>
2023-09-22 13:26:39 +0200

    Adds route to venue owners profile

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/home/Home";
 import VenuePage from "./pages/venue/VenuePage";
-import Register from "./pages/register/RegisterPage"
+import Register from "./pages/register/RegisterPage";
 import Login from "./pages/login/LoginPage";
 import ProfilePage from "./pages/profile/ProfilePage";
 import AddVenue from "./pages/venue/AddVenuePage";
 import UpdateVenue from "./pages/venue/UpdateVenuePage";
-
+import OwnerProfile from "./pages/profile/OwnerProfile";
 
 export default function App() {
   return (
@@ -20,6 +20,7 @@ export default function App() {
         <Route path="profilePage/:name" element={<ProfilePage />} />
         <Route path="addVenuePage" element={<AddVenue />} />
         <Route path="updateVenuePage/:id" element={<UpdateVenue />} />
+        <Route path="ownerProfile/:name" element={<OwnerProfile />} />
       </Route>
     </Routes>
   );

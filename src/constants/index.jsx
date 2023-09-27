0f88575fae31623ee8b7fbc18e0b1bd83bd0b44c
martemoslet <martemoslet@gmail.com>
2023-09-27 13:53:21 +0200

    Adds constants

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,4 +1,9 @@
-export const API_HOLIDAZE_URL = "https://api.noroff.dev/api/v1/holidaze"
+export const API_HOLIDAZE_URL = "https://api.noroff.dev/api/v1/holidaze";
 export const BOOKINGS = "?_bookings=true";
+export const VENUES = "?_venues=true";
+export const OWNER = "?_owner=true";
 
-export const missingImg = "https://cdn.pixabay.com/photo/2022/12/14/13/43/snow-7655439_1280.png"
+export const missingImg =
+  "https://cdn.pixabay.com/photo/2022/12/14/13/43/snow-7655439_1280.png";
+export const missingProfileImg =
+  "https://cdn.pixabay.com/photo/2018/11/13/21/43/avatar-3814049_1280.png";

e58e713c36fb866e05c45edbdbf2d47bec5d4997
martemoslet <martemoslet@gmail.com>
2023-09-27 13:53:45 +0200

    removes console log

--- src/pages/profile/YourBookings.jsx
+++ src/pages/profile/YourBookings.jsx
@@ -5,46 +5,45 @@ import { Spinner } from "react-bootstrap";
 import { useEffect } from "react";
 import BookingsOnProfile from "../../components/profile/BookingsOnProfile";
 
-
 export default function YourBookings() {
-    const [bookings, setBookings] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
-    const [isError, setIsError] = useState(false);
-    let { name } = useParams();
-  
-    useEffect(() => {
-      async function getData(url) {
-        try {
-          setIsLoading(true);
-          setIsError(false);
-  
-          const response = await authFetch(url);
-          const json = await response.json();
-  
-          setBookings(json);
-        } catch (error) {
-          console.log(error);
-        } finally {
-          setIsLoading(false);
-        }
+  const [bookings, setBookings] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  let { name } = useParams();
+
+  useEffect(() => {
+    async function getData(url) {
+      try {
+        setIsLoading(true);
+        setIsError(false);
+
+        const response = await authFetch(url);
+        const json = await response.json();
+
+        setBookings(json);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
       }
-  
-      getData(`https://api.noroff.dev/api/v1/holidaze/profiles/${name}/bookings/?_venue=true`);
-    }, [name]);
-  
-    if (isLoading || !bookings) {
-      return <Spinner animation="border" role="status">
-              <span className="visually-hidden">Loading...</span>
-              </Spinner>;
     }
-  
-    if (isError) {
-      return <div>Error</div>;
-    }
-    console.log(bookings)
 
+    getData(
+      `https://api.noroff.dev/api/v1/holidaze/profiles/${name}/bookings/?_venue=true`
+    );
+  }, [name]);
 
-return (
-<BookingsOnProfile bookings={bookings} />
-)
+  if (isLoading || !bookings) {
+    return (
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    );
+  }
+
+  if (isError) {
+    return <div>Error</div>;
+  }
+
+  return <BookingsOnProfile bookings={bookings} />;
 }
